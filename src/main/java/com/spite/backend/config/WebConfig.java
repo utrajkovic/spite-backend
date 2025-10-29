1f5c3ecbfb706b972b6fdc55b1afc79f5f21cf2b
azzyku13 <uros.trajkovic.22@singimail.rs>
2025-10-29 12:04:07 +0100

    Modified WebConfig

--- src/main/java/com/spite/backend/config/WebConfig.java
+++ src/main/java/com/spite/backend/config/WebConfig.java
@@ -11,6 +11,9 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("*")
+                .allowCredentials(false);
     }
 }
